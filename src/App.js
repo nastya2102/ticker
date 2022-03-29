198651daa251c9c215d39b4a1828b6180ac215af
nastya2102 <anastasiakuznetsova289@gmail.com>
2022-03-29 14:40:15 +0300

    First commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Ticker from "./containers/Ticker";
+
+function App() {
+  return (
+    <div className="App">
+      <Ticker />
+    </div>
+  );
+}
+
+export default App;
